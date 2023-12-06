f75ba4d398595c8c99a5dcaf42f810e0fe7f432e
William Janelli <williamjanelli@yahoo.com>
2023-12-06 09:13:40 -0800

    newURL

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   const [name, setName] = useState([])
 
   useEffect(() => {
-    fetch('https://namestarter.onrender.com/generate')
+    fetch('https://namegen-6i2k.onrender.com/generate')
       .then((res) => res.json())
       .then((nme) => setName([nme]))
   }, [])
