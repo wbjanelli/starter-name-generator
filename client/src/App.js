335b40689738ed86853680c04587342aac0c93c1
William Janelli <williamjanelli@yahoo.com>
2023-12-06 09:10:52 -0800

    url

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   const [name, setName] = useState([])
 
   useEffect(() => {
-    fetch('https://enigmatic-shore-92552.herokuapp.com/generate')
+    fetch('https://namestarter.onrender.com/generate')
       .then((res) => res.json())
       .then((nme) => setName([nme]))
   }, [])
